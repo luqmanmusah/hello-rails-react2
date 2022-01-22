8e0ee48895524a555b5e9f2b2855e730339cc801
luqmanmusah <musahluqman@gmail.com>
2022-01-22 16:06:06 +0000

    Initial commit
    Create rails app
    
    Connet rails-react

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'messages/index'
+  get 'pages/home'
+  get 'pages/hello_react'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
