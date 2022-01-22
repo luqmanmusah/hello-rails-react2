5ccbd9a42420caf1f5c3f5decc819c2e11366231
luqmanmusah <musahluqman@gmail.com>
2022-01-22 21:22:40 +0000

    Add redux file

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-  get 'pages/home'
-  get 'pages/hello_react'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'components#index'
+  
+  get '*page', to: 'components#index', constraints: -> (req) do
+    !req.xhr? && req.format.html?
+  end
+
   namespace :api do
     namespace :v1 do
       resources :messages, only: %i[index]
       get 'randomMessage', to: 'messages#random_message'
-  # Defines the root path route ("/")
-  # root "articles#index"
+    end
+  end
 end
---
+++ db/migrate/20220108134606_create_messages.rb
@@ -0,0 +1,10 @@
+class CreateMessages < ActiveRecord::Migration[7.0]
+  def change
+    create_table :messages, if_not_exists: true do |t|
+      t.string :title
+      t.string :body
+
+      t.timestamps
+    end
+  end
+end
