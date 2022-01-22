8c17438371b7cf61f8536113f9f5dc8c7627488b
luqmanmusah <musahluqman@gmail.com>
2022-01-22 21:23:22 +0000

    Import root to application.js

--- app/javascript/react/hello_react.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import { App } from "./components/App/index.jsx";
-
-document.addEventListener("DOMContentLoaded", () => {
-    render(<App />, document.getElementById('react-components'));
-  });
