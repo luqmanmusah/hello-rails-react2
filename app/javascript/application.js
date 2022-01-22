ea2960f2ebf09cbdfeac26dde5ad9195d1e58d46
luqmanmusah <musahluqman@gmail.com>
2022-01-22 21:24:09 +0000

    Fix issues in component_helper

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import './react/hello_react'
+/* eslint-disable import/extensions */
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import '@hotwired/turbo-rails';
+// import "controllers"
+// import "./components/clock.jsx"
+import './components/App.jsx';
